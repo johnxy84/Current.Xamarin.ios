38837fec4959a5350a69e05750bd48f38aa474c2
johnxy84 <johnxy84@gmail.com>
2017-12-11 22:17:14 +0100

    First Commit

---
+++ Current.Xamarin.iOS/Current.cs
@@ -0,0 +1,24 @@
+﻿using UIKit;
+
+namespace Current.Xamarin.iOS
+{
+    public class Current
+    {
+        /// <summary>
+        /// Returns the current viewcontroller in the Hierachy of ViewControllers
+        /// </summary>
+        public static UIViewController CurrentViewController
+        {
+            get
+            {
+                var window = UIApplication.SharedApplication.KeyWindow;
+                var controller = window.RootViewController;
+                while (controller.PresentedViewController != null)
+                {
+                    controller = controller.PresentedViewController;
+                }
+                return controller;
+            }
+        }
+    }
+}
